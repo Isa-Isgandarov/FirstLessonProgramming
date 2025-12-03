3c72c0ee4d3121a30f92d0a5f8fcbe6b4140df9d
Isa Isgandarov <isa.isgenderov.1993@gmail.com>
2025-12-03 21:44:28 +0400

    Project initially added

---
+++ FirstLessonProgramming/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("Birinci ededi daxil edin");
+        int var1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ikinci ededi daxil edin");
+        int var2 = int.Parse(Console.ReadLine());
+
+        int sum = Sum(var1, var2);
+
+        Console.WriteLine("Cem budur: " + sum);
+    }
+
+    // iki verilən ədədi toplayan metod
+    public static int Sum(int var1, int var2)
+    {
+        return var1 + var2;
+    }
+}
